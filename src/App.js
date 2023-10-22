07f0afa2aa0229f6eac3ca7d2d008d81a161941e
unknown <naazfalak5119@gmail.com>
2023-10-22 15:36:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import "../src/components/styles.css";
+
+function App() {
+  const [data, setData] = useState([]);
+  const fetchData = () => {
+    fetch(`https://dummyjson.com/users`)
+      .then((response) => response.json())
+      .then((actualData) => {
+        console.log(actualData);
+        setData(actualData.users);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className=" container">
+      <tbody>
+        <tr>
+          <th>S.No</th>
+          <th>Profile</th>
+          <th>First name</th>
+          <th>Last name</th>
+          <th>Gender</th>
+          <th>Email</th>
+          <th>Username</th>
+          <th>Domain</th>
+          <th>IP</th>
+          <th>University</th>
+        </tr>
+
+        {data.map((item, index) => (
+          <tr key={index}>
+            <td>{item.id}</td>
+            <td>
+              <img src={item.image} alt="" height={100} />
+            </td>
+            <td>{item.firstName}</td>
+            <td>{item.lastName}</td>
+            <td>{item.gender}</td>
+            <td>{item.email}</td>
+            <td>{item.username}</td>
+            <td>{item.domain}</td>
+            <td>{item.ip}</td>
+            <td>{item.university}</td>
+          </tr>
+        ))}
+      </tbody>
+    </div>
+  );
+}
+
+export default App;
